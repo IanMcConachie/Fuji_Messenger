0b4cb5b5cea9670606bb4b785f0be5818322e4fd
Ian McConachie <email.mcconachie@gmail.com>
2022-11-04 14:40:33 -0700

    Added main.c

---
+++ main.c
@@ -0,0 +1,128 @@
+/*
+Fuji Messenger Main
+
+Author: Ian McConachie
+Date Created: October 16, 2022
+Last Modified: November 4, 2022
+
+Description:
+	Fuji Messenger is an instant messaging protocol that only allows messages in the form of haikus.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+
+// MACRO DEFINITIONS
+
+#define UNUSED __attribute__((unused))
+#define MSG_MAX 100
+
+
+// MODULAR FUNCTIONS
+
+int input_checker(int argc) {
+	int ret = 0;
+
+	// Check to see if correct number or args
+	if (argc == 4)
+		ret = 1;
+
+	return ret;
+}
+
+
+int is_haiku() {
+	return 1;
+}
+
+
+// MAIN FUNCTION
+
+int main(int argc, char **argv) {
+
+	// Variable declarations
+	int ret = EXIT_SUCCESS;
+	struct sockaddr_in dest_addr, my_addr;
+	int sockfd;
+	char *incoming, *outgoing;
+
+	char test[MSG_MAX] = "Hello World!\n";
+
+	// Parsing inputs and seeing if there are any errors
+	if (input_checker(argc) == 0) {
+		fprintf(stderr, "ERROR: Incorrect number of args\nEXPECTED: ./Fuji [dest. IP address] [dest. port #] [local port #]\n");
+		ret = EXIT_FAILURE;
+		goto EXIT;
+	}
+
+	unsigned long dest_ip = (unsigned long)atoi(argv[1]);
+	short dest_port = (short)atoi(argv[2]);
+	short src_port = (short)atoi(argv[3]);
+
+	if ((dest_ip == 0)||(dest_port == 0)||(src_port == 0)) {
+		fprintf(stderr, "ERROR: Incorrect number of args\nEXPECTED: ./Fuji [dest. IP address] [dest. port #] [local port #]\n");
+		ret = EXIT_FAILURE;
+		goto EXIT;
+	}
+
+
+	// Creating address structures for source and destination
+
+	dest_addr.sin_family = AF_INET;
+	dest_addr.sin_addr.s_addr = htonl(dest_ip);
+	dest_addr.sin_port = htons(dest_port);
+
+	my_addr.sin_family = AF_INET;
+	inet_aton("localhost", &my_addr.sin_addr);
+	my_addr.sin_port = htons(src_port);
+
+
+
+	// Creating the socket and binding to local port number
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		fprintf(stderr, "ERROR: Failed to create socket\n");
+		ret = EXIT_FAILURE;
+		goto EXIT;
+	}
+
+
+	if ((bind(sockfd, (const struct sockaddr *)&my_addr, sizeof(my_addr))) < 0) {
+		fprintf(stderr, "ERROR: Failed to bind to socket\n");
+		ret = EXIT_FAILURE;
+		goto EXIT;
+	}
+
+	outgoing = (char *)malloc(sizeof(char) * MSG_MAX);
+	incoming = (char *)malloc(sizeof(char) * MSG_MAX);
+
+	while(1) {
+		fprintf(stdout, "> ");
+		fgets(outgoing, MSG_MAX, stdin);
+		if (strcmp(outgoing, "r\n") == 0) {
+			fprintf(stdout, "here\n");
+			//recvfrom(sockfd, (void *)incoming, MSG_MAX, 0, NULL, NULL);
+			//fprintf(stdout, "%s", incoming);
+		} else if (strcmp(outgoing, "q\n") == 0) {
+			break;
+		} else {
+			sendto(sockfd, (void *)test, sizeof(test), 0, (const struct sockaddr *)&dest_addr, sizeof(dest_addr));
+		}
+	}
+
+	//recvfrom(sockfd, (void *)incoming, MSG_MAX, 0, NULL, NULL);	
+	//sendto(sockfd, (void *)&join, sizeof(join), 0, (const struct sockaddr *)&dest_addr, sizeof(srv_addr));
+
+	free(outgoing);
+	free(incoming);
+
+EXIT:
+	return ret;
+}
